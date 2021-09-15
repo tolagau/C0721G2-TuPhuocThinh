915437ce22ccf7b107f6f4c51b0ad96dc323987c
TuThinh198 <tolagau.98@gmail.com>
2021-09-15 17:25:03 +0700

    up lần 1

---
+++ Module 2/src/_11_stack_queue/exercise/reverse/ReverseIntAndStr.java
@@ -0,0 +1,37 @@
+package _11_stack_queue.exercise.reverse;
+import java.util.Arrays;
+
+import java.util.Stack;
+
+public class ReverseIntAndStr {
+    public static void reverseInt(int [] arrNum){
+        Stack<Integer> arrInter = new Stack();
+        for (int i = 0; i<arrNum.length;i++){
+            arrInter.push(arrNum[i]);
+        }
+        for (int i =0;i<arrNum.length;i++){
+            arrNum[i]=arrInter.pop();
+        }
+    }
+    public static String reverseStr(String string){
+        String[] str = string.split("");
+        Stack<String> arrStr = new Stack<>();
+        for (int i =0;i<str.length;i++){
+            arrStr.push(str[i]);
+        }
+        String newString = "";
+        for (int i = 0; i < str.length; i++) {
+            newString += arrStr.peek();
+            arrStr.pop();
+        }
+        return newString;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,5,3,5,6,7,23,4};
+        reverseInt(array);
+        System.out.println(Arrays.toString(array));
+        String string = "TuThinh";
+        System.out.println(reverseStr(string));
+    }
+}
--- Module 2/src/_test_btnhom_vs_bttuan/ReadNumber.java
+++ Module 2/src/_test_btnhom_vs_bttuan/ReadNumber.java
@@ -1,33 +1,5 @@
 package _test_btnhom_vs_bttuan;
 
 public class ReadNumber {
-    private static String[] oneDigit =
-            {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    private static String[]  oneDigitWithTy =
-            {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-    private static String[]  oneDigitWithTeen =
-            {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    private static final String SPACE = " ";
 
-    public static String read(int number) {
-
-        String result = "";
-        int firstNumber = number / 100;
-        int secondAndThirdNumber = number % 100;
-        int secondNumber =  secondAndThirdNumber / 10;
-        int thirdNumber = secondAndThirdNumber % 10;
-
-        if (firstNumber != 0) {
-            result += oneDigit[firstNumber] + SPACE + "hundred";
-        }
-
-        result += SPACE;
-        if (secondNumber == 1) {
-            result += oneDigitWithTeen[thirdNumber];
-        } else {
-            result += oneDigitWithTy[secondNumber] + SPACE + oneDigit[thirdNumber];
-        }
-
-        return result.replaceAll(" {2}", " ").trim();
-    }
 }
